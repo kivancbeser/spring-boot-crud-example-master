45bc25f3c6e151101b994035880b831b65174dda
Kivanc Enes Beser <beserkivanc@gmail.com>
2020-07-20 00:35:17 +0300

    Add Javadoc for AccountService

--- src/main/java/com/crud/example/service/AccountService.java
+++ src/main/java/com/crud/example/service/AccountService.java
@@ -12,30 +12,56 @@ public class AccountService {
     @Autowired
     private AccountRepository repository;
 
+    /** This method allows the account to be saving.
+     * @param account
+     * @return Account
+     */
     public Account saveAccount(Account account) {
         return repository.save(account);
     }
 
+    /** This method allows the list of account to be saving.
+     * @param accounts
+     * @return List of Account
+     */
     public List<Account> saveAccounts(List<Account> accounts) {
         return repository.saveAll(accounts);
     }
 
+    /** This method find All accounts in db and return list of accounts.
+     * @return List of Account
+     */
     public List<Account> getAccounts() {
         return repository.findAll();
     }
 
+    /**  This method find Account By Id
+     * @param id
+     * @return Account
+     */
     public Account getAccountById(int id) {
         return repository.findById(id).orElse(null);
     }
 
+    /**  This method find Account By Name
+     * @param name
+     * @return Account
+     */
     public Account getAccountByName(String name) {
         return repository.findByFirstname(name);
     }
 
+    /** This method delete Account By Id
+     * @param id
+     */
     public void deleteAccount(int id) {
         repository.deleteById(id);
     }
 
+    /** This method finding by Id which wants to updated Account(finding by Id) and update the Account
+     * @param account
+     * @return
+     */
     public Account updateAccount(Account account) {
         Account existingAccount = repository.findById(account.getId()).orElse(null);
         existingAccount.setFirstname(account.getFirstname());
